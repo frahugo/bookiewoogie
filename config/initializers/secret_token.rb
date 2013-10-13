82c8244d0404f5a71fe2ecb496bfe7b2d4f6fc57
Hugo Frappier <hugo@cekoya.com>
2013-10-13 12:46:44 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookiewoogie::Application.config.secret_key_base = 'eca5d3d94e14a38c17a317735de3f6e8049d1e521ef4bb70aab6a216ea4451b66b153fab906dd8e7e4c77f1d1809678119cb6ce9e890b10a898915eebd3ce7d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookiewoogie::Application.config.session_store :cookie_store, key: '_bookiewoogie_session'
